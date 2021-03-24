3bd5625a39c96e6b5aece6fe98c1e589e8683ce7
Ying Yin <78532009+YinYing3969@users.noreply.github.com>
2021-03-24 21:01:03 +0100

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,3 @@
 window.onload = function(){
-	console.log("Details determine success or failure, attitude determines everything！我的 GitHub：https://github.com/hackhu2019");
+	console.log("Details determine success or failure, attitude determines everything！");
 }
